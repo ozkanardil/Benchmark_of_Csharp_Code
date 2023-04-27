d5d7c2e9ae75989a6efcc6c328910998bc57b114
Özkan Ardil <ozkanardil@gmail.com>
2023-04-28 00:36:42 +0300

    Benchmark of string and StringBuilder

---
+++ src/Benchmarker/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var result = BenchmarkRunner.Run<Demo>();
+
+
+[MemoryDiagnoser]
+public class Demo
+{
+    [Benchmark]
+    public string GetFullStringNormally()
+    {
+        string output = "";
+
+        for (int i = 0; i < 1000; i++)
+        {
+            output += i;
+        }
+
+        return output;
+    }
+
+    [Benchmark(Baseline = true)]
+    public string GetFullStringStringBuilder()
+    {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            output.Append(i);
+        }
+
+        return output.ToString();
+    }
+}
