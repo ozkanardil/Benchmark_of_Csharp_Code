d6aa386eec57cfda5a5f71a21ef0ed98f6b235fb
Ozkan <ozkanardil@gmail.com>
2024-05-26 17:54:26 +0200

    String vs StringBuilder projects has been added

--- src/StringVsStringbuilder/Program.cs
+++ src/StringVsStringbuilder/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+Console.WriteLine("String vs StringBuilder!");
+
+var result = BenchmarkRunner.Run<TestClassStringVsStringBuilder>();
+
+[MemoryDiagnoser]
+public class TestClassStringVsStringBuilder
+{
+    private const int N = 1000;
+
+    [Benchmark]
+    public string StringConcatenation()
+    {
+        string result = "";
+        for (int i = 0; i < N; i++)
+        {
+            result += "Welcome";
+        }
+        return result;
+    }
+
+    [Benchmark(Baseline = true)]
+    public string StringBuilderConcatenation()
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < N; i++)
+        {
+            sb.Append("Welcome");
+        }
+        return sb.ToString();
+    }
+}
